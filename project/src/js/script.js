775d61840c4186076069c4460182049c5ac3b211
unknown <matvii-bosyi>
2025-04-15 19:04:28 +0300

    header

--- project/src/js/script.js
+++ project/src/js/script.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	searchInput.addEventListener('input', () => {
 		if (searchInput.value.length > 0) {
 			searchClear.classList.remove('hidden')
+			// searchClear.classList.add('flex')
 		} else {
+			// searchClear.classList.remove('flex')
 			searchClear.classList.add('hidden')
 		}
 	})
@@ -14,5 +16,12 @@ document.addEventListener('DOMContentLoaded', () => {
 		searchInput.value = ''
 		searchClear.classList.add('hidden')
 		searchInput.focus()
+	})
+
+	// Додаємо обробник для Alt + Enter
+	document.addEventListener('keydown', event => {
+		if (event.altKey && event.key === 'Enter') {
+			searchInput.focus()
+		}
 	})
 })
